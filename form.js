401dd263e20f80274b7be8790fb1fafea5c8fdd9
Manoj Kumar Sah <manoj23@navgurukul.org>
2024-04-02 12:07:25 +0530

    form assignment js add

--- form.js
+++ form.js
@@ -1,27 +1,27 @@
 
-document.getElementById('task_Form').addEventListener('submit', function(e) {
-e.preventDefault();
+document.getElementById('task_form').addEventListener('submit', function(e) {
+    e.preventDefault();
 
-// Get all the input values
-var firstName = document.getElementById('firstName').value;
-var lastName = document.getElementById('lastName').value;
-var managerFirstName = document.getElementById('managerFirstName').value;
-var managerLastName = document.getElementById('managerLastName').value;
-var taskName = document.getElementById('taskName').value;
-var taskId = document.getElementById('taskId').value;
-var status = document.querySelector('input[name="status"]:checked').value;
-var notes = document.getElementById('notes').value;
-var instructions = document.getElementById('instructions').value;
 
-// Display submitted data below the form
-var submittedData = document.getElementById('submittedData');
-submittedData.innerHTML = `
-    <h2>Submitted Data</h2>
-    <p><strong>Employee Name:</strong> ${firstName} ${lastName}</p>
-    <p><strong>Manager Name:</strong> ${managerFirstName} ${managerLastName}</p>
-    <p><strong>Task ID:</strong> ${taskId}</p>
-    <p><strong>Status:</strong> ${status}</p>
-    <p><strong>Additional Notes:</strong> ${notes}</p>
-    <p><strong>Instructions:</strong> ${instructions}</p>
-`;
+    let  firstName = document.getElementById('firstName').value;
+    let  lastName = document.getElementById('lastName').value;
+    let  managerFirstName = document.getElementById('managerFirstName').value;
+    let  managerLastName = document.getElementById('managerLastName').value;
+    let  taskName = document.getElementById('taskName').value;
+    let  taskId = document.getElementById('taskId').value;
+    let  status = document.querySelector('input[name="Status"]:checked').value;
+    let  notes = document.getElementById('notes').value;
+    let  instructions = document.getElementById('instructions').value;
+
+
+    let  submittedData = document.getElementById('submittedData');
+    submittedData.innerHTML = `
+        <h2>Submitted Data</h2>
+        <p><strong>Employee Name:</strong> ${firstName} ${lastName}</p>
+        <p><strong>Manager Name:</strong> ${managerFirstName} ${managerLastName}</p>
+        <p><strong>Task ID:</strong> ${taskId}</p>
+        <p><strong>Status:</strong> ${status}</p>
+        <p><strong>Additional Notes:</strong> ${notes}</p>
+        <p><strong>Instructions:</strong> ${instructions}</p>
+    `;
 });
